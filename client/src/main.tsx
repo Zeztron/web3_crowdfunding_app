97824666697851f1c380e72263c5ffc0680e02fa
Zeztron <pateltharsh@gmail.com>
2022-12-10 18:38:22 -0600

    added all basic structure of components and pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
-import App from './components/App';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
